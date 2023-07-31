617e46bbcb95514e4d97c402652a901452123660
huseyingulec <itshuseyingulec@gmail.com>
2023-07-31 17:45:15 +0200

    add js file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const accounts = [
+const data = [
     {
         id: 1,
         title: "Main Account",
@@ -49,6 +49,77 @@ const accounts = [
         id: 3,
         title: "Savings",
         balance: "36500.12",
-        spendings: []
+        spendings: [
+            {
+                category: "Retiring fund",
+                spent: "35000"
+            }
+        ]
     }
 ]
+
+const accountFeed = document.querySelector(".account-feed");
+
+let listAccounts = ""
+
+for(let i=0; i<data.length; i++){
+    listAccounts += `
+    <div class="account-items" data-id ="${data[i].id}" id = "${data[i].id}" >
+        <h3>${data[i].title}</h3>
+        <h3>$${data[i].balance}</h3>
+    </div>
+    `
+}
+accountFeed.innerHTML = listAccounts
+
+const spendingItems = document.querySelector(".spending-items")
+const spendingFeed = document.querySelector(".spending-feed")
+
+listSpendings = ""
+
+document.addEventListener("click", event => {
+    if(event.target.dataset.id == 1) {
+
+        for(let i=0; i<data[0].spendings.length; i++){
+            listSpendings += `
+            <div class="spending-items">
+                <h3>${data[0].spendings[i].category}</h3>
+                <h3>$${data[0].spendings[i].spent}</h3>
+            </div>`
+        }
+
+        spendingFeed.innerHTML = listSpendings
+        listSpendings = ""
+
+    } 
+    if (event.target.dataset.id == 2) {
+
+        for(let i=0; i<data[1].spendings.length; i++){
+            listSpendings += `
+            <div class="spending-items">
+                <h3>${data[1].spendings[i].category}</h3>
+                <h3>$${data[1].spendings[i].spent}</h3>
+            </div>
+            `
+        }
+
+        spendingFeed.innerHTML = listSpendings
+        listSpendings = ""
+
+    } 
+    if (event.target.dataset.id == 3) {
+
+        for(let i=0; i<data[2].spendings.length; i++){
+            listSpendings += `
+            <div class="spending-items">
+                <h3>${data[2].spendings[i].category}</h3>
+                <h3>$${data[2].spendings[i].spent}</h3>
+            </div>
+            `
+        }
+
+        spendingFeed.innerHTML = listSpendings
+        listSpendings = ""
+
+    }
+})
